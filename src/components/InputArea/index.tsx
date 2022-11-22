f23364ff20c9ad88d577f18a721e73ff0b961a02
jhonatan Esteferson <Jhonatanjhonatan907@gmail.com>
2022-11-22 17:27:10 -0300

    Primeiro commit

---
+++ src/components/InputArea/index.tsx
@@ -0,0 +1,89 @@
+import * as C from './styles';
+import { Item } from '../../types/Item';
+import { FormEvent, useState } from 'react';
+
+type Props = {
+    onAdd: (item: Item) => void;
+}
+
+export const InputArea = ({ onAdd }: Props) => {
+    const [Data, setData_at] = useState<Date>();
+    const [cate, setCate] = useState('');
+    const [title, setTitle] = useState('');
+    const [value, setValue] = useState<Number>();
+
+    const handleAddEvent = () => {
+        let newItem: Item = {
+            date: new Date(2022, 10, 27),
+            category: 'food',
+            title: 'Pizza Mestre',
+            value: 120.50
+        };
+        onAdd(newItem);
+    }
+
+    function cadastrarDados(e: any) {
+        e.preventDefault()
+        console.log("teste no campo de data", Data);
+        console.log("teste no campo de data", cate);
+        console.log(title);
+        console.log(value);
+        
+    }
+
+
+    return (
+        <C.Container>
+            <form onSubmit={cadastrarDados}>
+                <C.Button onClick={handleAddEvent}>
+                    Adicionar
+                </C.Button>
+
+                <C.Date>
+                    <label htmlFor="Data">Data:</label>
+                    <input
+                        type="date"
+                        name="Data"
+                        id="Data"
+                        onChange={(e) => setData_at(new Date(e.target.value))}
+                    />
+                </C.Date>
+
+                <C.Cate>
+                    <label htmlFor="cate">Categoria</label>
+                    <input
+                        type="text"
+                        name="cate"
+                        id="cate"
+                        onChange={(e) => setCate((e.target.value))}
+                    />
+                </C.Cate>
+
+                <C.Title>
+                    <label htmlFor='title'>Title</label>
+                    <input 
+                    type="text" 
+                    name="title" 
+                    id="title" 
+                    onChange={(e) => setTitle((e.target.value))}
+                    />
+                </C.Title>
+
+                <C.Value>
+                    <label htmlFor="value">R$:</label>
+                    <input 
+                    type="number" 
+                    name='value' 
+                    id='value'
+                    onChange={(e) => setValue(new Number(e.target.value))}
+                     />
+                </C.Value>
+
+            </form>
+
+
+
+
+        </C.Container>
+    );
+}
---
+++ src/components/InputArea/styles.ts
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+
+export const Container = styled.div`
+      background-color: #FFF;
+      box-shadow: 0px 0px 5px #CCC;
+      border-radius: 10px;
+      padding: 20px;
+      margin-top: 20px;
+      display: flex;
+      align-items: center;
+      
+     
+`;
+
+export const Button = styled.button`
+     display: flex;
+     margin-top: 0;
+     font-size: 20px;
+     margin-right: 30px;
+     background-color: blue;
+     color: #FFF;
+     border-radius 5px;
+`;
+
+export const Date = styled.div`
+     flex: 2;
+     font-weight: bold;
+     margin-right: 30px;
+     color: #000; 
+     width: 40px; 
+    
+`;
+
+export const Cate = styled.div`
+
+      flex: 3;
+      font-weight: bold;
+      color: #000;
+      width: 40px;     
+`;
+
+export const Title = styled.div`
+       flex: 2;
+       margin-right: 30px;
+       font-weight: bold;
+       color: #000;
+       
+
+`;
+
+export const Value = styled.div`
+          flex: 3;
+          margin-right: 10px;
+          margin-top: 15px;
+          font-weight: bold;
+          color: #000
+`;
